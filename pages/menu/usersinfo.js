16e7614f95f9a8e06e0513f51abc9d40ff93a261
Liliia Chuiko <chuiko1112@gmail.com>
2022-07-05 16:22:25 +0100

    test

--- pages/menu/usersinfo.js
+++ pages/menu/usersinfo.js
@@ -186,7 +186,7 @@ export async function getServerSideProps(ctx) {
       },
     };
   } catch (e) {
-    console.log(e.message, 6787)
+    console.log(e.message, 67872)
     return {
       notFound: true,
     };
